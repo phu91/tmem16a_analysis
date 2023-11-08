fd702f98261f3cda0ddf1114b6f41e25b67c7e3e
Phu K. Tang <91493995+phu91@users.noreply.github.com>
2023-11-08 12:24:48 -0500

    plot_corr to plot generalized corelation

--- plot_corr.py
+++ plot_corr.py
@@ -4,9 +4,10 @@ import pandas as pd
 import seaborn as sns 
 import argparse
 from matplotlib import rc
+# import plotly.express as px
 
 sns.set_context("notebook")
-
+plt.style.use("dark_background")
 parser = argparse.ArgumentParser(description='Optional app description')
 
 parser.add_argument('--input', type=str, default='',
@@ -15,34 +16,61 @@ parser.add_argument('--input', type=str, default='',
 args = parser.parse_args()
 ifile = args.input
 
-data = []
-f = open ( ifile , 'r')
-lines = f.readline()    # SKIP THE FIRST LINE
-meta_info = lines.split(' ')
-print(len(rows))
+with open(ifile ,'r') as f:
+    lines = f.readline()    # SKIP THE FIRST LINE
+    # print(lines)
+    meta_info = lines.split(' ')
 
-rows = meta_info[0]
-cols = meta_info[1]
+    rows = int(meta_info[0])
+    cols = int(meta_info[2])
+    data = []
+    lines = f.readlines()
+    for line in lines:
+        data.extend(line.split())
 
-print(len(rows))
-# lines = f.readlines()
-# for line in lines:
-#     data.extend(line.split())
+    data.pop(-1)    # REMOVE THE LAST ']'
+    # print(len(data))
+    data = np.array(data,dtype=np.float64)
+    matrix = np.reshape(data,(rows,cols))
 
-# data.pop(-1)    # REMOVE THE LAST ']'
-# # print(len(data))
-# data = np.array(data)
-# data_float = data.astype(np.float)
-# # print(data_float)
-# matrix = np.reshape(data_float,(rows,cols))
-# print(matrix)
-# ### MISCELLANEOUS ###
-# plt.suptitle("%s"%(ifile[:-4]),va='top')
-# plt.rcParams['ps.useafm'] = True
-# rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-# plt.rcParams['pdf.fonttype'] = 42
-# plt.gcf().set_size_inches(7.5,4)   ## Wide x Height
-# # plt.locator_params(axis='both', nbins=5)
-# # plt.tight_layout()
-# plt.savefig("KDE%s"%(ifile[:-3]))
-# plt.show()
+resid_a = np.arange(73,928)
+resid_b = np.arange(73,928)
+resid_list = np.concatenate((resid_a,resid_b))
+df = pd.DataFrame(matrix,columns=resid_list,index=resid_list)
+
+# df_short = df.loc[224]
+# df_short = df.loc[[73,100],:]
+df_short = df
+fig,ax = plt.subplots(1,1)
+g = sns.heatmap(data=df_short,
+                # vmin=0,
+                # vmax=1,
+                robust=True,
+                cmap='RdPu_r')
+g.set_xlabel("RESID")
+g.set_ylabel("RESID")
+
+# fig = px.imshow(matrix,x=resid_list,y=resid_list,text_auto=True)
+# fig.show()
+# g = ax.imshow(matrix)
+
+# ax.set_xticks(np.arange(0,1710))
+# ax.set_yticks(np.arange(0,1710))
+
+# ax.set_xticklabels(resid_list,rotation=45)
+# ax.set_yticklabels(resid_list,)
+# fig.colorbar(g)
+
+
+### MISCELLANEOUS ###
+plt.locator_params(axis='y', nbins=30)
+plt.locator_params(axis='x', nbins=30)
+plt.suptitle("%s"%(ifile[:-4]),va='top')
+plt.rcParams['ps.useafm'] = True
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+plt.rcParams['pdf.fonttype'] = 42
+plt.gcf().set_size_inches(7.5,6)   ## Wide x Height
+# plt.locator_params(axis='both', nbins=5)
+# plt.tight_layout()
+plt.savefig("KDE%s"%(ifile[:-3]))
+plt.show()
--- rechain_bendix.tcl
+++ rechain_bendix.tcl
@@ -1,5 +1,5 @@
-mol new PBC_3A0B_R1.psf
-mol addfile PBC_CENTERED_PROT_3A0B_R1.xtc step 10 waitfor all
+#mol new PBC_3A0B_R1.psf
+#mol addfile PBC_CENTERED_PROT_3A0B_R1.xtc step 10 waitfor all
 set a [atomselect top "segname PROA"]
 set b [atomselect top "segname PROB"]
 $a set chain A
